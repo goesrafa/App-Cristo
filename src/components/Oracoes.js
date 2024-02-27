96e89e2e605bc473b334d2ff904b49668fcf5e6a
goesrafa <rafa.goes.2012@gmail.com>
2024-02-27 16:52:34 -0300

    Inicio da tela de orações

---
+++ src/components/Oracoes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+
+const Oracoes = () => {
+    return (
+        <View style={styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+});
+
+export default Oracoes;
